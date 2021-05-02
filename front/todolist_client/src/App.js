9f13fb9fd55583e2a5f2b2247f6aa8b5e2a804e6
stambouli <aminestambouli90@yahoo.fr>
2021-05-02 22:44:12 +0200

    Create the structure of the todo list and create form

--- front/todolist_client/src/App.js
+++ front/todolist_client/src/App.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [id, setId] = useState(1);
+  const [id, setId] = useState('');
   const [todo, setTodo] = useState(null);
+  const [name, setTName] = useState('');
+  const [categoty, setCategory] = useState('');
+  const [date, setDate] = useState('');
   useEffect(() => {
     axios.get('http://localhost:5000/alltodo').then((response) => {
       console.log(response.data);
@@ -14,6 +17,41 @@ function App() {
       console.log(err);
     });
   }, []);
+  const handleName = (event) => {
+    setTName(event.target.value);
+  };
+  const handleCategory = (event) => {
+    setCategory(event.target.value);
+  };
+  const handleDate = (event) => {
+    setDate(event.target.value);
+  };
+  const formSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      name,
+      categoty,
+      date,
+    };
+    console.log(data);
+    axios.post('http://localhost:5000/alltodo', data).then(() => {
+      setTName('');
+      setCategory('');
+      setDate('');
+    });
+  };
+  const handleId = (event) => {
+    setId(event.target.value);
+  };
+  const deleteTodo = (event) => {
+    event.preventDefault();
+    const todoId = parseInt(id, 10);
+    axios.delete(`http://localhost:5000/alltodo?id=${todoId}`).then(() => {
+
+    }).catch((err) => {
+      throw err;
+    });
+  };
 
   return todo !== null ? (
     <div className="App">
@@ -22,17 +60,32 @@ function App() {
         <div>
           {
             todo.map((item) => (
-              <div className="todoList">
+              <form className="todoList" onSubmit={deleteTodo}>
                 <div className="id" style={{ width: '5px' }}>{item.id}</div>
                 <input type="checkbox" />
                 <div style={{ width: '50px' }}>{item.categoty}</div>
                 <div style={{ width: '200px' }}>{item.name}</div>
                 <div style={{ width: '50px' }}>{item.date}</div>
-                <button style={{ width: '50px' }} type="button">Delete</button>
-              </div>
+                <button type="submit" value={item.id} onClick={handleId}>Delete</button>
+              </form>
             ))
           }
         </div>
+        <form className="addTodo" onSubmit={formSubmit}>
+          <div className="radioTodo">
+            <label htmlFor="radio1">Administration</label>
+            <input type="radio" value="Administration" onChange={handleCategory} name="category" id="radio1" />
+            <label htmlFor="radio2">Famille</label>
+            <input type="radio" value="Famille" onChange={handleCategory} name="category" id="radio2" />
+            <label htmlFor="radio3">Achat</label>
+            <input type="radio" value="Achat" onChange={handleCategory} name="category" id="radio3" />
+          </div>
+          <div className="inputInfos">
+            <input type="text" value={name} onChange={handleName} id="inputName" />
+            <input type="text" value={date} onChange={handleDate} id="inputDate" />
+          </div>
+          <button type="submit">Ajouter</button>
+        </form>
       </div>
     </div>
   ) : (
