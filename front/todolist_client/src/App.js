4dd791aafca8ae88f5801ac41d4d6da4c5fcfbbe
stambouli <aminestambouli90@yahoo.fr>
2021-05-03 00:27:31 +0200

    Change style and add bootsrap

--- front/todolist_client/src/App.js
+++ front/todolist_client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   const [todo, setTodo] = useState(null);
   const [name, setTName] = useState('');
   const [date, setDate] = useState('');
+  const [lineDecoration, setLineDecoration] = useState('none');
   useEffect(() => {
     axios.get('http://localhost:5000/alltodo').then((response) => {
       console.log(response.data);
@@ -16,6 +18,9 @@ function App() {
       console.log(err);
     });
   }, []);
+  const handleLineDecoration = () => {
+    setLineDecoration('line-through');
+  };
   const handleName = (event) => {
     setTName(event.target.value);
   };
@@ -53,25 +58,24 @@ function App() {
       <div className="todo">
         <h1>MA TODO LIST</h1>
         <div>
-          {
-            todo.map((item) => (
-              <form className="todoList" onSubmit={deleteTodo}>
-                <div className="id" style={{ width: '5px' }}>{item.id}</div>
-                <input type="checkbox" />
-                <div style={{ width: '50px' }}>{item.categoty}</div>
-                <div style={{ width: '300px' }}>{item.name}</div>
-                <div style={{ width: '50px' }}>{item.date}</div>
-                <button type="submit" value={item.id} onClick={handleId}>Delete</button>
-              </form>
-            ))
-          }
           <form className="addTodo" onSubmit={formSubmit}>
             <div className="inputInfos">
               <input type="text" value={name} onChange={handleName} id="inputName" placeholder="Ajouter un todo à la liste" />
               <input type="text" value={date} onChange={handleDate} id="inputDate" placeholder="Precisez la date" />
-              <button type="submit">Ajouter</button>
+              <button type="submit" className="btn btn-success">Ajouter</button>
             </div>
           </form>
+          {
+            todo.map((item) => (
+              <form className="todoList" onSubmit={deleteTodo}>
+                <div className="id" style={{ width: '5px' }}>{item.id}</div>
+                <input type="checkbox" onChange={handleLineDecoration} />
+                <div value={item.id} style={{ textDecorationLine: `${lineDecoration}` }} id="todoName">{item.name}</div>
+                <div style={{ width: '50px' }}>{item.date}</div>
+                <button type="submit" value={item.id} onClick={handleId} className="btn btn-danger">Delete</button>
+              </form>
+            ))
+          }
         </div>
       </div>
     </div>
