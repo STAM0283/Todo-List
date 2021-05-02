62dfe98910b8f5bae1043bea496a63de7ae0245d
stambouli <aminestambouli90@yahoo.fr>
2021-05-02 14:49:05 +0200

    Create Route Post

--- back/todolist_api/routes/todolist.js
+++ back/todolist_api/routes/todolist.js
@@ -6,11 +6,33 @@ const connexion = require('../data/mysql');
 router.get('/alltodo', (req, res) => {
   try {
     connexion.query('SELECT * FROM allTodo', (err, result) => {
-      if (err) console.log(err);
-      res.send({
-        message: 200,
-        result,
-      });
+      if (err) {
+        console.log(err);
+      } else {
+        res.send({
+          message: 200,
+          result,
+        });
+      }
+    });
+  } catch {
+    console.log('erreur');
+  }
+});
+router.post('/alltodo', (req, res) => {
+  const { name } = req.body;
+  const { categoty } = req.body;
+  const { date } = req.body;
+  try {
+    connexion.query('INSERT INTO allTodo (name, categoty, date) VALUES (?, ?, ?)', [name, categoty, date], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send({
+          status: 200,
+          message: result,
+        });
+      }
     });
   } catch {
     console.log('erreur');
