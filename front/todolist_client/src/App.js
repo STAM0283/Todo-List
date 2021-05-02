54fd8872c61bd03d7c36968180e1e7b67f742ad5
stambouli <aminestambouli90@yahoo.fr>
2021-05-02 17:22:15 +0200

    Create todolist template

--- front/todolist_client/src/App.js
+++ front/todolist_client/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
+  const [id, setId] = useState(1);
+  const [todo, setTodo] = useState(null);
+  useEffect(() => {
+    axios.get('http://localhost:5000/alltodo').then((response) => {
+      console.log(response.data);
+      setTodo(response.data);
+      setId(id + 1);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  return todo !== null ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo">
+        <h1>MA TODO LIST</h1>
+        <div>
+          {
+            todo.map((item) => (
+              <div className="todoList">
+                <div className="id" style={{ width: '5px' }}>{item.id}</div>
+                <input type="checkbox" />
+                <div style={{ width: '50px' }}>{item.categoty}</div>
+                <div style={{ width: '200px' }}>{item.name}</div>
+                <div style={{ width: '50px' }}>{item.date}</div>
+                <button style={{ width: '50px' }} type="button">Delete</button>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
+  ) : (
+    <p>LOADING</p>
   );
 }
 
